a9252c916618a06b3aef34bce8f3b3fae82801f7
foggybrains <dustin.keyes@gmail.com>
2019-08-19 12:54:14 -0600

    deployment

--- src/Components/TaskListContainer.js
+++ src/Components/TaskListContainer.js
@@ -3,10 +3,12 @@ import TaskList from './TaskList'
 
 export default function TaskListContainer(props) {
 
+    console.log(props)
+
     return(
         <div className='sub-container'>
-            <h1>{props.user.name.toUpperCase()}'s Task List</h1>
-            < TaskList tasks={props.tasks}/>
+            <h1>'s Task List</h1>
+            {/* < TaskList tasks={props.tasks}/> */}
             <button>New Task</button>
             <button>New Timer</button>
         </div>
